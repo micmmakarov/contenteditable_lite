9efd37138e527a5dff00dd1a08462625e707a780
Michael Makarov <info1117@gmail.com>
2012-09-06 20:04:30 -0700

    the first version of my gem

--- test/dummy/app/controllers/application_controller.rb
+++ test/dummy/app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  helper Contenteditable::Engine.helpers
+  helper_method :hello_world
 
+  contenteditable_filter :hello_world
+
+  def hello_world
+
+   #true
+    false
+    #binding.pry
+
+  end
 
 end
